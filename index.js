f5a92c24a6a5d0f385ebeed68a8738ccad4511a2
david p <davidperrai@gmail.com>
2016-12-07 15:16:00 +0100

    correction des nom de varables

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'))
 
-.get('/b/:borne/e/:entity/n/:nfc_id', function(request, response) {
-  
-  var borne  = request.params.borne  || request.body.borne; 
-  var entity = request.params.entity || request.body.entity;
+.get('/b/:borne_id/e/:entity_id/n/:nfc_id', function(request, response) {
+
+  var borne_id  = request.params.borne_id  || request.body.borne_id; 
+  var entity_id = request.params.entity_id || request.body.entity_id;
   var nfc_id = request.params.nfc_id || request.body.nfc_id;
 
-  io.emit('nfc_readed', {borne: borne, nfc_id: nfc_id,entity: entity });
+  io.emit('nfc_readed', {borne_id: borne_id, nfc_id: nfc_id,entity_id: entity_id });
 });
 
 io.on('connection', function(){ 
