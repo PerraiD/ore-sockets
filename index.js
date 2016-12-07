9098057d9013c19f95e458c349cb414affe35b9c
david p <davidperrai@gmail.com>
2016-12-07 15:09:17 +0100

    ajout de l'identifiant d'entity

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'))
 
-.get('/:borne/:nfc_id', function(request, response) {
-
-  var borne  = request.params.borne || request.body.borne;
+.get('/b/:borne/e/:entity/n/:nfc_id', function(request, response) {
+  
+  var borne  = request.params.borne  || request.body.borne; 
+  var entity = request.params.entity || request.body.entity;
   var nfc_id = request.params.nfc_id || request.body.nfc_id;
 
-  io.emit('nfc_readed', {borne: borne, nfc_id: nfc_id });
+  io.emit('nfc_readed', {borne: borne, nfc_id: nfc_id,entity: entity });
 });
 
 io.on('connection', function(){ 
